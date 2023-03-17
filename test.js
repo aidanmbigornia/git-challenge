c6c0d0dfa491c46d8e13e1e542ebc3cae2c06d2f
aidanmbigornia <aidanmbigornia@gmail.com>
2023-03-18 05:01:21 +0800

    feat: add base64 endpoint

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const tape = require('tape')
 const jsonist = require('jsonist')
 
-const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
+const PORT = process.env.PORT = process.env.PORT || require('get-port-sync')()
 const server = require('./server')
 
 const urlBase = `http://localhost:${PORT}`
@@ -9,7 +9,6 @@ const urlBase = `http://localhost:${PORT}`
 tape('should respond hello', (t) => {
   jsonist.get(urlBase, (err, body) => {
     if (err) t.error(err)
-
     t.equal(body.msg, 'hello')
     t.end()
   })
@@ -19,8 +18,12 @@ tape('should respond user-agent', (t) => {
   const opts = { headers: { 'User-Agent': 'tape' } }
   jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
     if (err) t.error(err)
-
     t.equal(body.ua, 'tape')
+tape('should respond b64', (t) => {
+  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.b64, 'aGVsbG8=')
     t.end()
   })
 })
@@ -28,4 +31,6 @@ tape('should respond user-agent', (t) => {
 tape('cleanup', function (t) {
   server.close()
   t.end()
+})
+})
 })
